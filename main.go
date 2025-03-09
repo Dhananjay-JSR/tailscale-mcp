26acecc5b2add1c969694c748ba8baf40f2d9cf6
Dhananjay-JSR <ajaysenday@gmail.com>
2025-03-09 12:00:18 +0530

    feat(oauth): Add token validation and automatic token refresh

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 
 const tailscale_api_base_url = "https://api.tailscale.com/api/v2"
 
+const default_tailnet_name = "-"
+
 var oauth_token OauthToken
 
 type OauthToken struct {
@@ -49,6 +51,33 @@ func GetOauthToken(client_id string, client_secret string) string {
 	return oauth_token.AccessToken
 }
 
+func IsOauthTokenValid(token string) bool {
+	req, err := http.NewRequest(http.MethodGet, tailscale_api_base_url+"/tailnet/"+default_tailnet_name+"/devices", nil)
+	if err != nil {
+		return false
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false
+	}
+	if resp.StatusCode != 200 {
+		return false
+	}
+	return true
+}
+
+// This function will always make sure to return a valid token
+func GetToken(client_id string, client_secret string) string {
+	token := GetOauthToken(client_id, client_secret)
+	if IsOauthTokenValid(token) {
+		return token
+	}
+	oauth_token.AccessToken = ""
+	return GetToken(client_id, client_secret)
+}
+
 func main() {
 	client_id := flag.String("client_id", "", "Client ID of the Tailscale Oauth Client")
 	client_secret := flag.String("client_secret", "", "Client Secret of the Tailscale Oauth Client")
@@ -56,6 +85,6 @@ func main() {
 	if *client_id == "" || *client_secret == "" {
 		log.Fatal("Client ID and Secret are required")
 	}
-	data := GetOauthToken(*client_id, *client_secret)
-	fmt.Println(data)
+	token := GetToken(*client_id, *client_secret)
+	fmt.Println(token)
 }
